a3412b9a3d3d6238c5c0a61213987f1c2696d65a
Bernardo Sunderhus <5483269+bsunderhus@users.noreply.github.com>
2018-12-13 15:40:16 -0200

    Adds  valid license (#32)

--- src/config/types.ts
+++ src/config/types.ts
@@ -2,3 +2,14 @@ export enum Category {
   PROJECTS = 'projects',
   LIBRARIES = 'libraries'
 }
+
+export enum License {
+  Apache = 'Apache',
+  BSD = 'BSD',
+  CreativeCommonsBY = 'CreativeCommonsBY',
+  EclipsePublic = 'EclipsePublic',
+  ISC = 'ISC',
+  MIT = 'MIT',
+  WTFPL = 'WTFPL',
+  Yahoo = 'Yahoo'
+}
