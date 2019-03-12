61971d58a84feb24bac9df4d6508f2cbab9f497f
Peter Varholak <peter.varholak@gmail.com>
2019-03-12 16:14:35 +0100

    change: update package.json

--- types/react-dom/index.d.ts
+++
@@ -1,9 +0,0 @@
-import ReactDOM from 'react-dom'
-
-declare module 'react-dom' {
-  export function createRoot(
-    root: HTMLElement
-  ): {
-    render: (element: React.ReactElement, callback?: () => void) => void
-  }
-}
