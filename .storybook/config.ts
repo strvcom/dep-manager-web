cd6b1d07f5324a1c6f4d7391b6c6ef6b62971f00
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-04-04 19:28:58 -0300

    docs: added static styles to storybook; added styled-component theme context

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,6 +1,19 @@
-import { configure } from '@storybook/react'
+import { configure, addDecorator } from '@storybook/react'
 import 'babel-polyfill'
+import { withThemes } from 'storybook-styled-components'
 
+// then import your themes
+import light from '../src/styles/themes/light'
+import dark from '../src/styles/themes/dark'
+
+const themes = {
+  Light: light,
+  Dark: dark
+}
+
+// now add the decorator
+addDecorator(withThemes(themes))
+
+// load stories.
 const req = require.context('../src', true, /\.stories\.(js|jsx|ts|tsx)$/)
-
 configure(() => req.keys().forEach(filename => req(filename)), module)
