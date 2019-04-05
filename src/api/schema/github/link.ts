60f4b8ba67973fb6793ea90ffab9b1ec63033394
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-04-05 17:04:38 -0300

    refactor: moved GitHub link to its own file

---
+++ src/api/schema/github/link.ts
@@ -0,0 +1,32 @@
+import { ApolloLink } from 'apollo-link'
+import { HttpLink } from 'apollo-link-http'
+import fetch from 'isomorphic-fetch'
+
+import { debug } from '../../debug'
+import { GITHUB_OAUTH_TOKEN } from '../../../config/env'
+
+/**
+ * Fixed authorization method, used only for local development of the API.
+ * On the application itself, authentication is managed using Netlify auth
+ * providers with a local storing of tokens.
+ */
+const authorization = GITHUB_OAUTH_TOKEN
+  ? `bearer ${GITHUB_OAUTH_TOKEN}`
+  : undefined
+
+if (authorization) {
+  debug('Using fixed authentication token %o', authorization)
+}
+
+const httpLink = new HttpLink({
+  uri: 'https://api.github.com/graphql',
+  fetch,
+  headers: { authorization }
+})
+
+// @TODO: implement.
+const authLink = new ApolloLink((operation, forward) => forward!(operation))
+
+const link = ApolloLink.from([authLink, httpLink])
+
+export { link }
