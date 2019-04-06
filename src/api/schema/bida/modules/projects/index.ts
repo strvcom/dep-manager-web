ac79fc8a7be079603d2bdf6edd01de376b5c3e65
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-04-06 18:34:27 -0300

    feat: added Repository::departments field

--- src/api/schema/bida/modules/projects/index.ts
+++ src/api/schema/bida/modules/projects/index.ts
@@ -1,14 +1,13 @@
 import gql from 'graphql-tag'
 
+const DEPARTMENTS = ['FRONTEND', 'BACKEND', 'ANDROID', 'IOS']
+
 const typeDefs = gql`
   """
   Enumerator that indicates types of projects this application acknowledges
   """
   enum BidaDepartment {
-    FRONTEND
-    BACKEND
-    ANDROID
-    IOS
+    ${DEPARTMENTS.join('\n')}
   }
 
   type NPMPackage {
@@ -21,6 +20,7 @@ const typeDefs = gql`
 
   extend type Repository {
     npmPackage: NPMPackage
+    departments: [BidaDepartment]
   }
 
   extend type Query {
@@ -49,17 +49,16 @@ const projects = (root: any, args: any, context: any, info: any) => {
   const { department, ...search } = args
   const { schema, mergeInfo } = info
 
+  const type = 'REPOSITORY'
+  const query = `topic:${department.toLowerCase()} user:strvcom`
+
   return mergeInfo.delegateToSchema({
     info,
     schema,
     context,
     operation: 'query',
     fieldName: 'search',
-    args: {
-      type: 'REPOSITORY',
-      query: `topic:${department.toLowerCase()} user:strvcom`,
-      ...search
-    }
+    args: { type, query, ...search }
   })
 }
 
@@ -80,9 +79,28 @@ const npmPackage = {
       : null
 }
 
+const departments = {
+  fragment: `
+    ... on Repository {
+      repositoryTopics (first: 10) {
+        nodes {
+          topic {
+            name
+          }
+        }
+      }
+    }
+  `,
+  resolve: ({ repositoryTopics }: any) =>
+    repositoryTopics.nodes
+      .map(({ topic: { name } }: any) => name)
+      .map((name: string) => name.toUpperCase())
+      .filter((name: string) => DEPARTMENTS.includes(name))
+}
+
 const resolvers = {
   Query: { projects },
-  Repository: { npmPackage }
+  Repository: { npmPackage, departments }
 }
 
 export default { typeDefs, resolvers }
