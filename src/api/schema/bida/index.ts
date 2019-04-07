48b64fe3c807d7809a3108b704080d9b23e75be2
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-04-07 13:48:28 -0300

    feat: added typings to Bida schema

--- src/api/schema/bida/index.ts
+++ src/api/schema/bida/index.ts
@@ -1,16 +1,30 @@
 import { print } from 'graphql/language'
+import { IResolvers } from 'graphql-tools'
 import { over, lensProp, mergeDeepRight } from 'ramda'
 
 import npm from './modules/npm'
 import projects from './modules/projects'
 
-const modules = [npm, projects]
+interface Module {
+  typeDefs: any // AST
+  resolvers: IResolvers
+}
 
-const schema = modules.map(over(lensProp('typeDefs'), print)).reduce(
-  (acc, next): any => ({
-    typeDefs: `${acc.typeDefs}\n${next.typeDefs}`,
-    resolvers: mergeDeepRight(acc.resolvers, next.resolvers)
-  })
-)
+const modules: Module[] = [npm, projects]
+
+/**
+ * Combines two GraphQL modules (typeDefs + resolvers) into one.
+ */
+const combine = (left: Module, right: Module): Module => ({
+  // combine typeDefs strings into one.
+  typeDefs: `${left.typeDefs}\n${right.typeDefs}`,
+  // combine resolvers into one.
+  resolvers: mergeDeepRight(left.resolvers, right.resolvers)
+})
+
+const schema = modules
+  // extract typeDefs text, AST is not compatible
+  .map(over(lensProp('typeDefs'), print))
+  .reduce(combine)
 
 export { schema }
