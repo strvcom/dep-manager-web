a215ac9c746c470211a3b14ee4f3c27a10b9228b
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-04-07 13:25:24 -0300

    cleanup: removed previous authentication state handling (local state based)

--- src/config/apolloClient.ts
+++ src/config/apolloClient.ts
@@ -1,33 +1,9 @@
 import { ApolloClient } from 'apollo-client'
-import { AUTH_QUERY } from '../data/Auth'
-import link, { cache, GITHUB_TOKEN_KEY } from './link'
-import { AuthQuery } from '../data/Auth/__generated-types/AuthQuery'
+
+import link, { cache } from './link'
 import typeDefs from '../data/typeDefs'
 import resolvers from './resolvers'
 
-const client = new ApolloClient({
-  link,
-  cache,
-  typeDefs,
-  resolvers
-})
-
-client.writeQuery({
-  query: AUTH_QUERY,
-  data: {
-    authentication: {
-      __typename: 'BidaAuthentication',
-      token: localStorage.getItem(GITHUB_TOKEN_KEY)
-    }
-  }
-} as any)
-
-client
-  .watchQuery<AuthQuery>({ query: AUTH_QUERY })
-  .subscribe(({ data: { authentication } }) => {
-    if (authentication && authentication.token) {
-      localStorage.setItem(GITHUB_TOKEN_KEY, authentication.token)
-    } else localStorage.removeItem(GITHUB_TOKEN_KEY)
-  })
+const client = new ApolloClient({ link, cache, typeDefs, resolvers })
 
 export default client
