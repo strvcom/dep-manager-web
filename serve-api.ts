76065c5fd75fff67a302fde4235644dc4f0293e3
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-04-08 00:50:29 -0300

    refactor: moved GITHUB_OAUTH_TOKEN usage logic out to serve-api script

--- serve-api.ts
+++ serve-api.ts
@@ -9,11 +9,21 @@ import { schema } from './src/api/schema'
 
 const running = ({ url }: { url: string }) => console.log(`API ready at ${url}`)
 
+/**
+ * Fixed authorization method, used only for local development of the API.
+ * On the application itself, authentication is managed using Netlify auth
+ * providers with a local storing of tokens.
+ */
+if (!process.env.GITHUB_OAUTH_TOKEN) {
+  throw new Error(
+    `You must define GITHUB_OAUTH_TOKEN env in order to start the dedicated API`
+  )
+}
+
 const config = {
   schema,
-  engine: {
-    apiKey: process.env.ENGINE_API_KEY
-  }
+  context: { token: process.env.GITHUB_OAUTH_TOKEN },
+  engine: { apiKey: process.env.ENGINE_API_KEY }
 }
 
 new ApolloServer(config).listen().then(running)
--- src/api/schema/github/link/auth.ts
+++ src/api/schema/github/link/auth.ts
@@ -1,30 +1,15 @@
 import { ApolloLink } from 'apollo-link'
 
-import { debug } from '../../../debug'
-import { GITHUB_OAUTH_TOKEN } from '../../../../config/env'
-
-/**
- * Fixed authorization method, used only for local development of the API.
- * On the application itself, authentication is managed using Netlify auth
- * providers with a local storing of tokens.
- */
-const authorization = GITHUB_OAUTH_TOKEN
-  ? `bearer ${GITHUB_OAUTH_TOKEN}`
-  : undefined
-
-if (authorization) {
-  debug('Using fixed authentication token %o', authorization)
-}
-
 const link = new ApolloLink((operation, forward) => {
   const { graphqlContext: { token } = { token: null } } = operation.getContext()
 
-  operation.setContext({
-    headers: {
-      // either use token, or global authentication method.
-      authorization: token ? `bearer ${token}` : authorization
-    }
-  })
+  if (token) {
+    operation.setContext({
+      headers: {
+        authorization: `bearer ${token}`
+      }
+    })
+  }
 
   return forward!(operation)
 })
