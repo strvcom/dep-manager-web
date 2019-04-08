41b7108f9dcddf10820d1c81b6e6090f9d5e872b
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-04-08 17:25:53 -0300

    test: added getLibraries tests

--- src/routes/Dashboard/helpers.test.ts
+++ src/routes/Dashboard/helpers.test.ts
@@ -3,6 +3,8 @@ import 'jest'
 import { __get__ } from './helpers'
 
 const setter = __get__('setter')
+const getLibraries = __get__('getLibraries')
+const getLibraries = __get__('getLibraries')
 
 describe('routes/Dashboard/helpers', () => {
   describe('setter', () => {
@@ -12,6 +14,32 @@ describe('routes/Dashboard/helpers', () => {
 
       expect(setter('b', mapper, obj)).toEqual({ a: '1', b: '2' })
       expect(mapper).toHaveBeenCalledWith(obj)
+    })
+  })
+
+  describe('getLibraries', () => {
+    const data = {
+      nil: {},
+      empty: { npmPackage: { dependencies: [] } },
+      fulfiled: {
+        npmPackage: {
+          dependencies: [
+            { package: { name: 'first' } },
+            { package: { name: 'second' } }
+          ]
+        }
+      }
+    }
+
+    it('should return empty array when no dependencies', () => {
+      expect(getLibraries(data.nil)).toEqual([])
+      expect(getLibraries(data.empty)).toEqual([])
+    })
+
+    it('should return dependency packages', () => {
+      expect(getLibraries(data.fulfiled).length).toBe(2)
+      expect(getLibraries(data.fulfiled)).toHaveProperty('0.name', 'first')
+      expect(getLibraries(data.fulfiled)).toHaveProperty('1.name', 'second')
     })
   })
 })
