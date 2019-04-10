3954a235aaeb0ae25313e0994aca82d1ab5e2d5a
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-04-10 20:04:06 -0300

    cleanup: apollo-utils; data; hooks

--- src/hooks/firstDayOfMonth.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function useFirstDayOfMonth () {
-  const now = new Date()
-  return React.useMemo(() => new Date(now.getFullYear(), now.getMonth(), 1), [
-    now.getFullYear(),
-    now.getMonth()
-  ])
-}
