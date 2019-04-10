d95d6a85af9d08365b74d24c10171b31287d48fc
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-04-10 09:57:01 -0300

    feat: added memo link to GitHub ApolloLink chain

--- src/api/schema/github/link/index.ts
+++ src/api/schema/github/link/index.ts
@@ -2,11 +2,12 @@ import { ApolloLink } from 'apollo-link'
 import { DedupLink } from 'apollo-link-dedup'
 
 import { link as auth } from './auth'
+import { link as memo } from './memo'
 import { link as http } from './http'
 import { link as debug } from './debug'
 
 const dedup = new DedupLink()
 
-const link = ApolloLink.from([dedup, auth, debug, http])
+const link = ApolloLink.from([dedup, auth, memo, debug, http])
 
 export { link }
