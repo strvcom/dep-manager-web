dc4e547f0cb4b1b8633dd7414a99e325e2772a85
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-04-10 09:54:59 -0300

    feat: added dedup link to GitHub ApolloLink chain

--- src/api/schema/github/link/index.ts
+++ src/api/schema/github/link/index.ts
@@ -1,9 +1,12 @@
 import { ApolloLink } from 'apollo-link'
+import { DedupLink } from 'apollo-link-dedup'
 
 import { link as auth } from './auth'
 import { link as http } from './http'
 import { link as debug } from './debug'
 
-const link = ApolloLink.from([auth, debug, http])
+const dedup = new DedupLink()
+
+const link = ApolloLink.from([dedup, auth, debug, http])
 
 export { link }
