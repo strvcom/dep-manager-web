b1fc64f87c126684c5f19ee806b0b934ced01a90
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-04-10 22:56:46 -0300

    feat: added search to Dashboard pages

--- src/routes/Dashboard/DashboardToolBar.tsx
+++ src/routes/Dashboard/DashboardToolBar.tsx
@@ -1,13 +1,20 @@
-import React, { Fragment, memo } from 'react'
+import React, { Fragment, memo, useState } from 'react'
 import ToolBar, { ToolBarLink } from '../../components/ToolBar'
 import { Input } from './styled'
 
 interface DashboardToolBarProps {
   department: string
   category: string
+  search: string
+  setSearch: (input: string) => void
 }
 
-const DashboardToolBar = ({ department, category }: DashboardToolBarProps) => (
+const DashboardToolBar = ({
+  department,
+  category,
+  search,
+  setSearch
+}: DashboardToolBarProps) => (
   <ToolBar
     title='Dashboard'
     links={
@@ -16,7 +23,14 @@ const DashboardToolBar = ({ department, category }: DashboardToolBarProps) => (
         <ToolBarLink to={`/${department}/projects`}>Projects</ToolBarLink>
       </Fragment>
     }
-    children={<Input placeholder={`Search ${category}`} />}
+    children={
+      <Input
+        autoFocus
+        value={search}
+        onChange={e => setSearch(e.target.value)}
+        placeholder={`Search ${category}`}
+      />
+    }
   />
 )
 
