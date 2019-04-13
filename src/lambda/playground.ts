41043475fce6209a8597c57e305ec73b2ec5dc34
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-04-13 16:02:15 -0300

    feat: added REACT_APP_GRAPHQL_ENDPOINT to control GraphQL API URI

--- src/lambda/playground.ts
+++ src/lambda/playground.ts
@@ -1,11 +1,20 @@
 import playground from 'graphql-playground-middleware-lambda'
+import { GITHUB_OAUTH_TOKEN, REACT_APP_GRAPHQL_ENDPOINT } from '../config/env'
 
 const headers: { [key: string]: string } = {}
 
-if (process.env.GITHUB_OAUTH_TOKEN) {
-  headers.authorization = `bearer ${process.env.GITHUB_OAUTH_TOKEN}`
+if (!REACT_APP_GRAPHQL_ENDPOINT) {
+  throw new Error(
+    `You must define REACT_APP_GRAPHQL_ENDPOINT env in order to GraphQL Playground`
+  )
 }
 
-const handler = playground({ endpoint: '/graphql', headers })
+// fulfil headers for Playground requests. Not strictly required, can be
+// altered through the Playground Settings UI.
+if (GITHUB_OAUTH_TOKEN) {
+  headers.authorization = `bearer ${GITHUB_OAUTH_TOKEN}`
+}
+
+const handler = playground({ endpoint: REACT_APP_GRAPHQL_ENDPOINT, headers })
 
 export { handler }
