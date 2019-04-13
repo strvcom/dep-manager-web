5d59899794cb047d7d80bdf99f3c9e494e8e3c2a
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-04-13 15:22:48 -0300

    feat: updated github auth link to use authorization from header

--- src/api/schema/github/link/auth.ts
+++ src/api/schema/github/link/auth.ts
@@ -1,14 +1,12 @@
 import { ApolloLink } from 'apollo-link'
 
 const link = new ApolloLink((operation, forward) => {
-  const { graphqlContext: { token } = { token: null } } = operation.getContext()
+  const {
+    graphqlContext: { headers }
+  } = operation.getContext()
 
-  if (token) {
-    operation.setContext({
-      headers: {
-        authorization: `bearer ${token}`
-      }
-    })
+  if (headers && headers.authorization) {
+    operation.setContext({ headers: { authorization: headers.authorization } })
   }
 
   return forward!(operation)
