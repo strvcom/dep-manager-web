fc87ef089b9cb8587ebe3ef3c9cb7ba072881b70
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-04-13 15:25:47 -0300

    refactor: removed memoization link from github, avoid data leak

--- src/api/schema/github/link/index.ts
+++ src/api/schema/github/link/index.ts
@@ -2,12 +2,11 @@ import { ApolloLink } from 'apollo-link'
 import { DedupLink } from 'apollo-link-dedup'
 
 import { link as auth } from './auth'
-import { link as memo } from './memo'
 import { link as http } from './http'
 import { link as debug } from './debug'
 
 const dedup = new DedupLink()
 
-const link = ApolloLink.from([dedup, auth, memo, debug, http])
+const link = ApolloLink.from([dedup, auth, debug, http])
 
 export { link }
