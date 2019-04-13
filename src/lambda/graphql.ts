5e6e4545223cab3a1244b0122d6389840c9a87a7
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-04-13 15:21:03 -0300

    feat: added /graphql lambda handler

---
+++ src/lambda/graphql.ts
@@ -0,0 +1,23 @@
+import { ApolloServer } from 'apollo-server-lambda'
+import { schema } from '../api/schema'
+
+const server = new ApolloServer({
+  schema,
+  introspection: true,
+  context: ({ event, context }: any) => ({
+    headers: event.headers,
+    functionName: context.functionName,
+    aws: { event, context }
+  }),
+  engine: { apiKey: process.env.ENGINE_API_KEY }
+})
+
+const handler = server.createHandler({
+  cors: {
+    origin: '*',
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }
+})
+
+export { handler }
