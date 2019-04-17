9952361367fcb347e1b51aa93a90a069f42a4a15
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-04-17 19:10:40 -0300

    chore: fixed newly introduced type errors on service worker

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -87,7 +87,10 @@ export function unregister () {
 export default function register () {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
-    const publicUrl = new URL(process.env.PUBLIC_URL, window.location as any)
+    const publicUrl = new URL(
+      process.env.PUBLIC_URL || '',
+      window.location as any
+    )
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
