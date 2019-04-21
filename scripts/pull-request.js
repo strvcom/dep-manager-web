e3fe8f0a1309101c86d8a050b332051b17f9e0f4
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-04-20 23:02:45 -0300

    refactor: rewrite pull-request script to use inquirer

--- scripts/pull-request.js
+++ scripts/pull-request.js
@@ -1,110 +1,142 @@
-const { execSync } = require('child_process')
+const { spawn } = require('child_process')
+const { prompt } = require('inquirer')
 const chalk = require('chalk')
-const { prompt } = require('enquirer')
+const ora = require('ora')
 
-const invariant = (bool, message) =>
-  !bool && (console.error(message), process.exit(1))
+const {
+  __,
+  equals,
+  prop,
+  pipe,
+  map,
+  match,
+  nth,
+  filter,
+  find,
+  values,
+  uniq
+} = require('ramda')
 
-try {
-  execSync('hub --version')
-} catch (err) {
-  invariant(
-    false,
-    'No GitHub cli available. You can install it from https://github.com/github/hub'
-  )
-}
+const git = require('simple-git/promise')()
 
-const getIssueNumber = branch => {
-  const match = branch.match(/issue\/([0-9]+)/)
-  return match && match[1]
-}
+const run = async () => {
+  const branches = await loader(git.branch(), 'Loading branches')
+  const current = getCurrentBranch(branches.branches)
 
-const current = execSync('git rev-parse --abbrev-ref HEAD')
-  .toString()
-  .trim()
+  const prompts = [
+    {
+      type: 'list',
+      name: 'remote',
+      message: 'Remote repository:',
+      choices: getRemotes(branches.all)
+    },
+    {
+      type: 'list',
+      name: 'base',
+      message: 'Target base branch:',
+      default: 'master',
+      choices: ({ remote }) => getRemoteBranches(remote)(branches.all)
+    },
+    {
+      type: 'list',
+      name: 'head',
+      message: 'Head branch:',
+      default: current,
+      choices: ({ remote }) => getRemoteBranches(remote)(branches.all)
+    },
+    {
+      type: 'input',
+      name: 'issue',
+      message: 'Target issue (empty for none)',
+      default: ({ head }) => getIssueNumber(head),
+      validate: issue =>
+        issue && !Number(issue) ? 'Issue must be a number (or empty)' : true
+    }
+  ]
 
-const coerceBranch = branch => {
-  try {
-    execSync(`git rev-parse --verify ${branch}`)
-  } catch (err) {
-    throw new Error(`An invalid branch was provided (${branch})`)
+  const { issue, head, base, remote } = await prompt(prompts)
+
+  await loader(sleep(), 'Processing input')
+
+  const args = ['pull-request', '-b', base, '-h', head]
+
+  if (issue) {
+    args.push('-i', issue)
+
+    console.log(
+      `Transform ${b(`issue #${issue}`)}\n  ...into a ${b(
+        'pull-request'
+      )}\n  ...from ${b(head)}\n  ...into ${b(base)}\n  ...at ${b(
+        remote
+      )}?\n\n${chalk.gray(
+        "This means the issue will be converted into a pull-request in an operation which can't be reverted.\n"
+      )}`
+    )
+
+    const { confirmed } = await prompt({
+      type: 'confirm',
+      name: 'confirmed',
+      message: 'Confirm',
+      default: false
+    })
+
+    if (!confirmed) process.exit()
   }
 
-  return branch
+  spawn('hub', args, { stdio: 'inherit' })
 }
 
-const { argv } = require('yargs')
-  .option('base', {
-    alias: 'b',
-    type: 'string',
-    describe: 'base branch against which to open the pull-request',
-    default: 'master',
-    coerce: coerceBranch
-  })
-  .option('head', {
-    alias: 'h',
-    type: 'string',
-    describe: 'head branch from which to open the pull-request',
-    default: current,
-    coerce: coerceBranch
-  })
-  .option('remote', {
-    alias: 'r',
-    type: 'string',
-    default: 'origin',
-    describe: 'remote origin against which to open the pull-request'
-  })
-  .option('issue', {
-    alias: 'i',
-    type: 'number',
-    describe:
-      'issue number. Defaults to extraction from branch name ("issue/[value]")',
-    default: current,
-    coerce: issue => {
-      const value = !Number(issue) ? getIssueNumber(issue) : issue
+/*
+ * Helpers
+ */
 
-      if (!value) {
-        throw new Error(`An invalid issue number was provided (${issue})`)
-      }
+const sleep = (ms = 500) => new Promise(resolve => setTimeout(resolve, ms))
 
-      return value
-    }
-  })
-
-const { base, head, remote, issue } = argv
+const loader = async (promise, text, min = 500) => {
+  console.log('')
+  const loading = Promise.all([promise, sleep(min)])
+  ora.promise(loading, text)
+  await loading
+  console.log('')
+  return promise
+}
 
 const b = text => chalk.white.bold(text)
 
-console.log(
-  `\nTransform ${b(`issue #${issue}`)}\n  ...into a ${b(
-    'pull-request'
-  )}\n  ...from ${b(head)}\n  ...into ${b(base)}\n  ...at ${b(
-    remote
-  )}?\n\n${chalk.gray(
-    "This means the issue will be converted into a pull-request in an operation which can't be reverted.\n"
-  )}`
+const parseBranch = match(/remotes\/([^/]*)\/(.*)/)
+
+const getRemotes = pipe(
+  map(parseBranch),
+  map(nth(1)),
+  filter(Boolean),
+  uniq
 )
 
-prompt({ type: 'confirm', name: 'confirmed', message: `Confirm` })
-  .then(({ confirmed }) => {
-    if (confirmed) {
-      const branches = execSync('git branch -a').toString()
+const getRemoteBranches = remote =>
+  pipe(
+    map(parseBranch),
+    // prettier-ignore
+    filter(pipe(nth(1), equals(remote))),
+    map(nth(2)),
+    uniq
+  )
 
-      invariant(
-        branches.match(`remotes/${remote}/${base}`),
-        `\nCould not find branch ${b(base)} on ${remote}.\n${b(
-          "Are you sure you've pushed it?\n"
-        )}`
-      )
+const getCurrentBranch = pipe(
+  values,
+  find(prop('current')),
+  prop('name')
+)
 
-      invariant(
-        branches.match(`remotes/${remote}/${head}`),
-        `\nCould not find branch ${b(head)} on ${remote}.\n${b(
-          "Are you sure you've pushed it?\n"
-        )}`
-      )
+const getIssueNumber = pipe(
+  match(/issue\/([0-9]+)/),
+  nth(1)
+)
 
-      execSync(`hub pull-request -i ${issue} -b ${base} -h ${head}`).toString()
-    }
+const childPromise = child =>
+  new Promise((resolve, reject) => {
+    child.addListener('error', reject)
+    child.addListener('exit', resolve)
   })
-  .catch(err => {})
+
+// start script
+run()
