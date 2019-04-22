c948e6001a10fab33626d6a28477c31bffffff1f
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-04-22 17:35:37 -0300

    chore: added storybook-react-router for react-router mocking on stories

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -1,8 +1,9 @@
-import { configure, addDecorator } from '@storybook/react'
 import 'babel-polyfill'
-import { withThemes } from 'storybook-styled-components'
 
-// then import your themes
+import { configure, addDecorator } from '@storybook/react'
+import { withThemes } from 'storybook-styled-components'
+import StoryRouter from 'storybook-react-router'
+
 import light from '../src/styles/themes/light'
 import dark from '../src/styles/themes/dark'
 
@@ -11,8 +12,8 @@ const themes = {
   Dark: dark
 }
 
-// now add the decorator
 addDecorator(withThemes(themes))
+addDecorator(StoryRouter())
 
 // load stories.
 const req = require.context('../src', true, /\.stories\.(js|jsx|ts|tsx)$/)
