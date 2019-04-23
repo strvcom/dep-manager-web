1398831ed914ec4949d171245be762e4aca8139f
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-04-23 10:52:13 -0300

    style: minor variable renaming

--- src/components/NodeLibrariesTable/NodeLibrariesTable.stories.tsx
+++ src/components/NodeLibrariesTable/NodeLibrariesTable.stories.tsx
@@ -27,21 +27,22 @@ storiesOf('NodeLibrariesTable', module)
     <NodeLibrariesTable libraries={[]} outdates={{ MAJOR: [], MINOR: [] }} />
   ))
   .add('filled', () => {
-    const libs = Math.max(0, number('Libraries', 10))
-    const libraries = new Array(libs).fill(null).map(chance.library)
-    const outs = { range: true, min: 0, max: libs, step: 1 }
+    const amount = Math.max(0, number('Libraries', 10))
+
+    const libraries = new Array(amount).fill(null).map(chance.library)
+    const outs = { range: true, min: 0, max: amount, step: 1 }
 
     const outdates = {
       MAJOR: chance
-        .pickset(libraries, Math.min(libs, 5), outs)
+        .pickset(libraries, Math.min(amount, 5), outs)
         .map(prop('package')),
 
       MINOR: chance
-        .pickset(libraries, Math.min(libs, 3), outs)
+        .pickset(libraries, Math.min(amount, 3), outs)
         .map(prop('package')),
 
       UPTODATE: chance
-        .pickset(libraries, Math.min(libs, 2), outs)
+        .pickset(libraries, Math.min(amount, 2), outs)
         .map(prop('package'))
     }
 
