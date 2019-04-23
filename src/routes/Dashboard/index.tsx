8ba58ec04cf478075c982e538fa956a89e59eefc
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-04-23 12:49:58 -0300

    refactor: renamed LibrariesActualityWidget to ActualityWidget

--- src/routes/Dashboard/index.tsx
+++ src/routes/Dashboard/index.tsx
@@ -7,7 +7,7 @@ import AuthenticatedQuery from '../../containers/AuthenticatedQuery'
 import Loading from '../../components/Loading'
 import NodeLibrariesTable from '../../components/NodeLibrariesTable'
 import NodeProjectsTable from '../../components/NodeProjectsTable'
-import ActualityWidget from '../../containers/LibrariesActualityWidget'
+import ActualityWidget from '../../containers/ActualityWidget'
 
 import ProjectsOverviewWidget from './ProjectsOverviewWidget'
 import RecentUpdates from './RecentUpdates'
--- src/routes/NodeLibraryDetails/index.tsx
+++ src/routes/NodeLibraryDetails/index.tsx
@@ -4,7 +4,7 @@ import { __, propEq } from 'ramda'
 
 import ToolBar from '../../components/ToolBar'
 import { Wrapper, Content, Sidebar, Input } from './styled'
-import ActualityWidget from '../../containers/LibrariesActualityWidget'
+import ActualityWidget from '../../containers/ActualityWidget'
 import NodeLibraryDependentsTable from '../../components/NodeLibraryDependentsTable'
 import { BidaDepartment } from '../../config/types'
 import Loading from '../../components/Loading'
