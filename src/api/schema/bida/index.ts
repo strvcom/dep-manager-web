9d391b568e2901160e2872ffb7091005d49932e2
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-04-23 16:40:13 -0300

    refactor: updated bida to use api/lib/modules helper

--- src/api/schema/bida/index.ts
+++ src/api/schema/bida/index.ts
@@ -1,32 +1,17 @@
 import { print } from 'graphql/language'
-import { IResolvers } from 'graphql-tools'
-import { over, lensProp, mergeDeepRight } from 'ramda'
+import { over, lensProp } from 'ramda'
+
+import { combine, Module } from '../lib/modules'
 
 import npm from './modules/npm'
 import npms from './modules/npms'
 import projects from './modules/projects'
 
-interface Module {
-  typeDefs: any // AST
-  resolvers: IResolvers
-}
-
 const modules: Module[] = [npm, npms, projects]
 
-/**
- * Combines two GraphQL modules (typeDefs + resolvers) into one.
- */
-const combine = (left: Module, right: Module): Module => ({
-  // combine typeDefs strings into one.
-  typeDefs: `${left.typeDefs}\n${right.typeDefs}`,
-  // combine resolvers into one.
-  resolvers: mergeDeepRight(left.resolvers, right.resolvers)
-})
-
 const schema = modules
-  // extract typeDefs text, AST is not compatible
+  // extract typeDefs text, AST is not compatible to "combine"
   .map(over(lensProp('typeDefs'), print))
-  // @ts-ignore
   .reduce(combine)
 
 export { schema }
