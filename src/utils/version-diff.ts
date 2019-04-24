5c357a146a73c8e2659188bf3cf4dcf3f5e49836
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-04-24 12:14:41 -0300

    cleanup: removed unused versionDiff helper

--- src/utils/version-diff.ts
+++ src/utils/version-diff.ts
@@ -16,15 +16,4 @@ const versionDistance = curry(
   }
 )
 
-const versionDiff = (left: string | SemVer, right: string | SemVer) => {
-  const coercedLeft = semver.coerce(left)
-  const coercedRight = semver.coerce(right)
-
-  if (!coercedLeft || !coercedRight) return null
-
-  return semver.diff(coercedLeft, coercedRight)
-}
-
-export default versionDiff
-
-export { versionDistance, versionDiff }
+export { versionDistance }
