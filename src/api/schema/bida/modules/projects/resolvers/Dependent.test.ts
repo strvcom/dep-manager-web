0cd2f146386a04e67c5e1b93f757e5444a292ede
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-04-24 10:08:03 -0300

    test: added Dependent::(version|name|id|outdateStatus) tests

--- src/api/schema/bida/modules/projects/resolvers/Dependent.test.ts
+++ src/api/schema/bida/modules/projects/resolvers/Dependent.test.ts
@@ -1,24 +1,51 @@
-import { __get__ } from './Dependent'
+import { Dependent, __get__ } from './Dependent'
 
 const getVersion = __get__('getVersion')
+const { version, name, id, outdateStatus } = Dependent
 
 deepDescribe('api/bida/projects/resolvers', () => {
   beforeEach(jest.clearAllMocks)
 
+  const repository = {
+    name: 'repo',
+    npmPackage: {
+      dependencies: [
+        { package: { name: 'first' }, version: '1.0.0' },
+        { package: { name: 'second' }, version: '2.0.0' }
+      ]
+    }
+  }
+
+  const __parent = { name: 'first', version: '0.9.0' }
+
   describe('getVersion', () => {
     it('should extract version from repository', () => {
-      const repository = {
-        name: 'name',
-        npmPackage: {
-          dependencies: [
-            { package: { name: 'first' }, version: '1.0.0' },
-            { package: { name: 'second' }, version: '2.0.0' }
-          ]
-        }
-      }
-
       expect(getVersion('first', repository)).toBe('1.0.0')
       expect(getVersion('second', repository)).toBe('2.0.0')
+    })
+  })
+
+  describe('Dependent::version', () => {
+    it('should resolve version', () => {
+      expect(version({ __parent, repository })).toBe('1.0.0')
+    })
+  })
+
+  describe('Dependent::name', () => {
+    it('should resolve name', () => {
+      expect(name({ repository })).toBe('repo')
+    })
+  })
+
+  describe('Dependent::id', () => {
+    it('should resolve id', () => {
+      expect(id({ __parent, repository })).toBe('1.0.0@repo')
+    })
+  })
+
+  describe('Dependent::outdateStatus', () => {
+    it('should resolve outdateStatus', () => {
+      expect(outdateStatus({ __parent, repository })).toBe('MAJOR')
     })
   })
 })
