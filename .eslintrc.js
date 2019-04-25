8d29846574525621d6bc9fb7d8448d716a82b4fc
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-04-24 22:45:51 -0300

    chore: added eslint dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+'use strict'
+
+module.exports = {
+  extends: [
+    '@strv/eslint-config-react',
+    '@strv/eslint-config-react/style',
+    '@strv/eslint-config-typescript',
+    '@strv/eslint-config-typescript/style'
+  ]
+}
