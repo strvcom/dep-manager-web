3038154bbe1b58634e260510a98076da905bd123
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-04-24 23:49:02 -0300

    style: ignore "react-hooks/exhaustive-deps" rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,5 +6,8 @@ module.exports = {
     '@strv/eslint-config-react/style',
     '@strv/eslint-config-typescript',
     '@strv/eslint-config-typescript/style'
-  ]
+  ],
+  rules: {
+    "react-hooks/exhaustive-deps": false
+  }
 }
