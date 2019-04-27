d34964f34d82c126e20aed8d6a76b914a7ce854a
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-04-26 23:32:51 -0300

    Revert "refactor: updated Dashboard to use gql import"
    This reverts commit 61f71bb5dfd5ae09a2f930034202c0f689fb6d96.

--- src/routes/Dashboard/index.tsx
+++ src/routes/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React, { Fragment, Suspense, memo, useState } from 'react'
 import { Route, RouteComponentProps, Switch } from 'react-router-dom'
+import gql from 'graphql-tag'
 import ErrorBoundary from 'react-error-boundary'
 
 import * as routes from '../routes'
@@ -14,7 +15,43 @@ import RecentUpdates from './RecentUpdates'
 import DashboardToolBar from './DashboardToolBar'
 import { TableContainer, StyledDashboard, WidgetContainer } from './styled'
 import { extractLibrariesInfo } from './helpers'
-import { DASHBOARD_QUERY } from './query.gql'
+
+const DASHBOARD_QUERY = gql`
+  query DASHBOARD_QUERY($department: BidaDepartment!) {
+    projects(first: 50, department: $department) {
+      total: repositoryCount
+      edges {
+        cursor
+        node {
+          ... on Repository {
+            id
+            name
+            pushedAt
+            npmPackage {
+              id
+              dependencies {
+                id
+                name
+                outdateStatus
+                package {
+                  id
+                  name
+                  version
+                  license
+                  updatedAt
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    archived: projects(department: $department, archived: true) {
+      total: repositoryCount
+    }
+  }
+`
 
 export interface Props
   extends RouteComponentProps<{
