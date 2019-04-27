1348f6a6a8503ed4700520f45da094810b5b3dda
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-04-26 23:32:51 -0300

    Revert "refactor: updated CurrentUserContainer to use gql import"
    This reverts commit a50306048957728b5d1e87fbc54058ce6b9419d1.

--- src/containers/CurrentUserContainer/index.tsx
+++ src/containers/CurrentUserContainer/index.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import gql from 'graphql-tag'
 
 import { CurrentUserContainerProps as Props, Result } from './types'
 import AuthenticatedQuery from '../AuthenticatedQuery'
-import { CURRENT_USER_QUERY } from './query.gql'
+
+const CURRENT_USER_QUERY = gql`
+  query CURRENT_USER_QUERY {
+    user: viewer {
+      id
+      name
+    }
+  }
+`
 
 const CurrentUserContainer = ({ children, ...rest }: Props) => (
   <AuthenticatedQuery query={CURRENT_USER_QUERY} {...rest}>
