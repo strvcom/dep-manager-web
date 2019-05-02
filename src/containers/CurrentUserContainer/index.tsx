9ff0bf31672be1183ce4ac711e536da3a2e9ced9
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-05-02 15:56:53 -0300

    refactor: improved typing for CurrentUserContainer

--- src/containers/CurrentUserContainer/index.tsx
+++ src/containers/CurrentUserContainer/index.tsx
@@ -1,38 +1,30 @@
 import React from 'react'
-import { QueryResult } from 'react-apollo'
+import { QueryResult, QueryProps } from 'react-apollo'
 
 import AuthenticatedQuery from '../AuthenticatedQuery'
-import { CURRENT_USER_QUERY } from './query.gql'
 
-interface IData {
-  user: { id: string, name: string } | undefined
-}
-
-interface IResult {
-  data: IData
-}
+import CURRENT_USER_QUERY, {
+  CURRENT_USER_QUERYQueryData,
+  CURRENT_USER_QUERYQueryPartialData,
+} from './query.gql'
 
 interface IChildrenResult
-  extends QueryResult<{
-    user?: object
-  }> {
-  user?: object
+  extends QueryResult,
+    CURRENT_USER_QUERYQueryPartialData {}
+
+interface IProps extends Pick<QueryProps, Exclude<keyof QueryProps, 'query'>> {
+  children: (result: IChildrenResult) => JSX.Element | null
 }
 
-type IChildren = (result: IChildrenResult) => React.ReactNode
-
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const CurrentUserContainer = ({
-  children,
-  ...rest
-}: {
-  children: IChildren
-}): JSX.Element => (
-  <AuthenticatedQuery query={CURRENT_USER_QUERY} {...rest}>
-    {({ data: { user } = { user: undefined }, ...result }: IResult) =>
+const CurrentUserContainer = ({ children, ...rest }: IProps): JSX.Element => (
+  <AuthenticatedQuery<CURRENT_USER_QUERYQueryData>
+    query={CURRENT_USER_QUERY}
+    children={({ data: { user } = { user: undefined }, ...result }) =>
       children({ user, ...result } as IChildrenResult)
     }
-  </AuthenticatedQuery>
+    {...rest}
+  />
 )
 
 export default CurrentUserContainer
