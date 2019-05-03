88bb95c64a541988edabb9b7df202925c9e44c9f
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-05-03 19:13:42 -0300

    refactor: added RecentUpdates graphql fragment based typings

---
+++ src/components/RecentUpdates/graphql-types/RecentUpdates_library.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL fragment: RecentUpdates_library
+// ====================================================
+
+export interface RecentUpdates_library {
+  __typename: "NPMPackage";
+  id: string;
+  name: string;
+  version: string;
+  updatedAt: string | null;
+}
