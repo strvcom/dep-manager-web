02f50fdf5673626255a4bbc6beadf548cbfadd70
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-05-04 14:25:58 -0300

    refactor: improved CurrentUserContainer types using inference

--- src/containers/CurrentUserContainer/index.tsx
+++ src/containers/CurrentUserContainer/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { QueryResult, QueryProps } from 'react-apollo'
+import { Omit } from 'utility-types'
 
 import AuthenticatedQuery from '../AuthenticatedQuery'
-
 import CURRENT_USER_QUERY from './query.gql'
 
 import {
@@ -10,21 +10,21 @@ import {
   CURRENT_USER_QUERY_user as IUser,
 } from './graphql-types/CURRENT_USER_QUERY'
 
-interface IChildrenResult extends QueryResult {
+interface IChildrenResult extends Omit<QueryResult<IData>, 'data'> {
   user?: IUser
 }
 
-interface IProps extends Pick<QueryProps, Exclude<keyof QueryProps, 'query'>> {
-  children: (result: IChildrenResult) => JSX.Element | null
+interface IProps extends Omit<QueryProps<IData>, 'query' | 'children'> {
+  children: (result: IChildrenResult) => React.ReactNode
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const CurrentUserContainer = ({ children, ...rest }: IProps): JSX.Element => (
+const CurrentUserContainer = ({ children, ...rest }: IProps) => (
   <AuthenticatedQuery<IData>
     {...rest}
     query={CURRENT_USER_QUERY}
     children={({ data: { user } = { user: undefined }, ...result }) =>
-      children({ user, ...result } as IChildrenResult)
+      children({ user, ...result })
     }
   />
 )
