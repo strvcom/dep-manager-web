131f7b6345cb97564601ac5a56968d77e882bd55
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-05-05 00:45:13 -0300

    refactor: updated github connections transform to use generic FieldTransform

--- src/api/schema/github/transforms/connections.ts
+++ src/api/schema/github/transforms/connections.ts
@@ -1,67 +1,19 @@
-import { parseType, GraphQLSchema, GraphQLObjectType } from 'graphql'
-import { ASTDefinitionBuilder } from 'graphql/utilities/buildASTSchema'
-import { visitSchema, VisitSchemaKind } from 'graphql-tools/dist/transforms/visitSchema'
+import { parseType, FieldDefinitionNode } from 'graphql'
 import { composeTransforms } from 'graphql-tools/dist/transforms/transforms'
 
-// type normalization
-
-type SchemaVisitor = { [key: string]: TypeVisitor }
-type TypeVisitor = (type: GraphQLObjectType, schema: GraphQLSchema) => GraphQLObjectType
-
-const visit = visitSchema as (schema: GraphQLSchema, visitor: SchemaVisitor) => GraphQLSchema
-
-/**
- * Generate an AST builder based on an existing schema.
- */
-const getBuilder = (schema: GraphQLSchema) =>
-  // @ts-ignore ASTDefinitionBuilder is wrongly typed in @types/graphql.
-  new ASTDefinitionBuilder({}, name => schema.getType(name))
-
-/**
- * Extract type name from type expression.
- */
-const getTypeName = (type: string) => {
-  const matched = type.match(/([a-z]*)/giu)
-
-  if (!matched) {
-    throw new Error(`Could not resolve type name from "${type}"`)
-  }
-
-  return matched[1]
-}
+import { FieldTransform, getTypeName } from '../../lib/transform'
 
 /*
  * Normalize connections to avoid empty edges/nodes (not realistic).
  */
-const transformEdgesFields = {
-  transformSchema: (schema: GraphQLSchema) => {
-    const builder = getBuilder(schema)
+const transformEdgesFields = new FieldTransform('edges', /Connection$/u, (field, { builder }) => {
+  const name = getTypeName(field.type)
 
-    return visit(schema, {
-      [VisitSchemaKind.OBJECT_TYPE]: (type: GraphQLObjectType): GraphQLObjectType => {
-        if (type.name.match(/Connection$/u)) {
-          const config = type.toConfig()
-          // parseType
-
-          if (!config.fields.edges) {
-            return type
-          }
-
-          const name = getTypeName(config.fields.edges.type.toString())
-
-          config.fields.edges = builder.buildField({
-            ...config.fields.edges.astNode,
-            type: parseType(`[${name}!]!`),
-          })
-
-          return new GraphQLObjectType(config)
-        }
-
-        return type
-      },
-    })
-  },
-}
+  return builder.buildField({
+    ...field.astNode as FieldDefinitionNode,
+    type: parseType(`[${name}!]!`),
+  })
+})
 
 const transform = composeTransforms(transformEdgesFields)
 
