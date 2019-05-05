3342dcf79ca304d381af7d380d4c426228aff7d0
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-05-05 01:15:29 -0300

    feat: added connections transformation to github schema

--- src/api/schema/github/index.ts
+++ src/api/schema/github/index.ts
@@ -1,7 +1,8 @@
 import { transformSchema } from 'graphql-tools'
 
 import { schema as originalSchema } from './schema'
+import { transform as transformConnections } from './transforms/connections'
 
-const schema = transformSchema(originalSchema, [])
+const schema = transformSchema(originalSchema, [transformConnections])
 
 export { schema }
