fe30bec7918bc64e3df8031ec350a73d1dc06fe5
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-06-26 20:09:50 -0300

    chore: added externals to lambda webpack

--- webpack.functions.js
+++ webpack.functions.js
@@ -1,6 +1,8 @@
 const Dotenv = require('dotenv-webpack')
+const externals = require('webpack-node-externals')
 require('dotenv/config')
 
 module.exports = {
+  externals: [externals()],
   plugins: [new Dotenv()]
 }
