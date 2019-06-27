70b87bfc4aa511049a4d205dabfbc176108bb062
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2019-06-26 21:23:23 -0300

    fix: avoid duplicated returns from playground lambda

--- src/lambda/playground.ts
+++ src/lambda/playground.ts
@@ -1,12 +1,11 @@
+import { Handler } from 'aws-lambda'
 import playground from 'graphql-playground-middleware-lambda'
 import { GITHUB_OAUTH_TOKEN, REACT_APP_GRAPHQL_ENDPOINT } from '../config/env'
 
 const headers: { [key: string]: string } = {}
 
 if (!REACT_APP_GRAPHQL_ENDPOINT) {
-  throw new Error(
-    `You must define REACT_APP_GRAPHQL_ENDPOINT env in order to GraphQL Playground`
-  )
+  throw new Error(`You must define REACT_APP_GRAPHQL_ENDPOINT env in order to GraphQL Playground`)
 }
 
 // fulfil headers for Playground requests. Not strictly required, can be
@@ -15,6 +14,13 @@ if (GITHUB_OAUTH_TOKEN) {
   headers.authorization = `bearer ${GITHUB_OAUTH_TOKEN}`
 }
 
-const handler = playground({ endpoint: REACT_APP_GRAPHQL_ENDPOINT, headers })
+const handle = playground({ endpoint: REACT_APP_GRAPHQL_ENDPOINT, headers })
+
+/**
+ * Simple wrapper to avoid duplicated response (callback + return).
+ */
+const handler: Handler = (lambdaEvent, context, callback) => {
+  handle(lambdaEvent, context, callback)
+}
 
 export { handler }
