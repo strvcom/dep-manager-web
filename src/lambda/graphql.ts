0cb23fd6ebd649ce90a52131292a9518523ee5ba
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-04-23 12:21:28 +0200

    refactor: upgrade apollo-server-lambda, and moved server creationn to src/api

--- src/lambda/graphql.ts
+++ src/lambda/graphql.ts
@@ -1,34 +1,4 @@
-import { ApolloServer } from 'apollo-server-lambda'
-import { schema } from '../api/schema'
-
-const { ENGINE_API_KEY } = process.env
-
-interface ILambdaEvent {
-  headers: {
-    [header: string]: string
-  }
-}
-
-interface ILambdaContext {
-  functionName: string
-}
-
-interface ILambdaArguments {
-  event: ILambdaEvent
-  context: ILambdaContext
-}
-
-const server = new ApolloServer({
-  schema,
-  introspection: true,
-  // eslint-disable-next-line no-shadow
-  context: ({ event, context }: ILambdaArguments) => ({
-    functionName: context.functionName,
-    aws: { event, context },
-  }),
-  // optional
-  engine: ENGINE_API_KEY ? { apiKey: ENGINE_API_KEY } : false,
-})
+import { server } from '../api/server'
 
 const handler = server.createHandler({
   cors: {
