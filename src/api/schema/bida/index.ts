9f152fb01a8f188b4f874ccd0f0950e40b149511
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-04-23 17:47:46 +0200

    refactor: moved schema combination logic to bida (only used there)

--- src/api/schema/bida/index.ts
+++ src/api/schema/bida/index.ts
@@ -1,8 +1,4 @@
-/* eslint-disable no-shadow */
-import { print } from 'graphql/language'
-import { over, lensProp } from 'ramda'
-
-import { combine, IGraphQLModule } from '../lib/modules'
+import { combine, IGraphQLModule } from './lib/modules'
 
 import npm from './modules/npm'
 import npms from './modules/npms'
@@ -10,9 +6,6 @@ import projects from './modules/projects'
 
 const modules: IGraphQLModule[] = [npm, npms, projects]
 
-const schema: IGraphQLModule = modules
-  // extract typeDefs text, AST is not compatible to "combine"
-  .map(over(lensProp('typeDefs'), print))
-  .reduce(combine)
+const schema = modules.reduce(combine)
 
 export { schema }
--- src/api/schema/bida/lib/modules.ts
+++ src/api/schema/bida/lib/modules.ts
@@ -1,19 +1,22 @@
 import { mergeDeepRight } from 'ramda'
 import { IResolvers } from 'graphql-tools'
+import { print, ASTNode } from 'graphql/language'
+// import { IResolverObject } from 'apollo-server-lambda'
 
 export interface IGraphQLModule {
   typeDefs?: string // printed AST
   resolvers?: IResolvers
 }
+
+const stringify = (typeDefs: string | ASTNode = '') =>
+  typeof typeDefs === 'string' ? typeDefs : print(typeDefs)
+
 /**
  * Combines two GraphQL modules (typeDefs + resolvers) into one.
  */
-const combine = (
-  left: IGraphQLModule,
-  right: IGraphQLModule
-): IGraphQLModule => ({
+const combine = (left: IGraphQLModule, right: IGraphQLModule): IGraphQLModule => ({
   // combine typeDefs strings into one.
-  typeDefs: `${left.typeDefs || ''}\n${right.typeDefs || ''}`,
+  typeDefs: `${stringify(left.typeDefs)} \n ${stringify(right.typeDefs)}`,
   // combine resolvers into one.
   resolvers: mergeDeepRight(left.resolvers || {}, right.resolvers || {}),
 })
