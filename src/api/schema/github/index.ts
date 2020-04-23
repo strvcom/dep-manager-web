86d7c1d15e81d4d10cbb52d7607b2b0fd4f3c5ca
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-04-23 17:43:58 +0200

    cleanup: removed GitHub schema transformations

--- src/api/schema/github/index.ts
+++ src/api/schema/github/index.ts
@@ -1,8 +1,1 @@
-import { wrapSchema } from 'graphql-tools'
-
-import { schema as originalSchema } from './schema'
-import { transforms as transformConnections } from './transforms/connections'
-
-const schema = wrapSchema(originalSchema, [...transformConnections])
-
-export { schema }
+export { schema } from './schema'
