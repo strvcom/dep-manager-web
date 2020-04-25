461c348b68e5ac94b18a1e2cf64068ecd728049e
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-04-25 18:48:17 +0200

    chore: added graphql-resolver typings

---
+++ src/@types/graphql-resolvers/index.d.ts
@@ -0,0 +1,29 @@
+declare module 'graphql-resolvers' {
+  import { ResolverFn } from '~api/types'
+
+  export const skip: undefined
+
+  export interface TArgs {
+    [argument: string]: any
+  }
+
+  export function combineResolvers<TSource = any, TContext = any>(
+    ...resolvers: Array<ResolverFn<any, TSource, TContext, TArgs>>
+  ): ResolverFn<any, TSource, TContext, TArgs>
+
+  export function pipeResolvers<TSource = any, TContext = any>(
+    ...resolvers: Array<ResolverFn<any, TSource, TContext, TArgs>>
+  ): ResolverFn<any, TSource, TContext, TArgs>
+
+  export function allResolvers<TSource = any, TContext = any>(
+    resolvers: Array<ResolverFn<any, TSource, TContext, TArgs>>
+  ): ResolverFn<any, TSource, TContext, TArgs>
+
+  export function resolveDependee(dependeeName: string): ResolverFn<any, any, any, TArgs>
+
+  export function resolveDependees(dependeeNames: string[]): ResolverFn<any, any, any, TArgs>
+
+  export function isDependee<TSource = any, TContext = any>(
+    resolver: ResolverFn<any, TSource, TContext, TArgs>
+  ): ResolverFn<any, TSource, TContext, TArgs>
+}
