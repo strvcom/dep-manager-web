fa84995a95e74c8c41e96f9b1634637c4ce2a3f4
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-04-28 16:00:03 +0200

    fix: fixed type usage on version diff lib

--- src/utils/version-diff.ts
+++ src/utils/version-diff.ts
@@ -6,17 +6,17 @@ import { is } from '~app/utils/type-utils'
 const isSemVerStatus = is(SemverOutdateStatus)
 
 const versionDistance = curry((left: string | SemVer, right: string | SemVer) => {
-  if (!left || !right) return SemverOutdateStatus.Unknown
+  if (!left || !right) return SemverOutdateStatus.UNKNOWN
 
   const coercedLeft = semver.coerce(left)
   const coercedRight = semver.coerce(right)
 
-  if (!coercedLeft || !coercedRight) return SemverOutdateStatus.Unknown
+  if (!coercedLeft || !coercedRight) return SemverOutdateStatus.UNKNOWN
 
   const distance = semver.diff(coercedLeft, coercedRight)?.toUpperCase()
-  if (distance) return isSemVerStatus(distance) ? distance : SemverOutdateStatus.Unknown
+  if (distance) return isSemVerStatus(distance) ? distance : SemverOutdateStatus.UNKNOWN
 
-  return SemverOutdateStatus.Uptodate
+  return SemverOutdateStatus.UPTODATE
 })
 
 export { versionDistance }
