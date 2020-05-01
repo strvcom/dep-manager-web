72cd9e4558326e47bc27325c58704b5a27f61a1a
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-05-01 11:36:33 +0200

    docs: documented hooks

--- src/api/client/hooks.ts
+++ src/api/client/hooks.ts
@@ -13,6 +13,31 @@ import { useEffect } from 'react'
 type Queries = OperationMap['query']
 type Mutations = OperationMap['mutation']
 
+/**
+ * This is a enhanced version of Apollo's useQuery with the following purposes:
+ *
+ * 1 - Less verbose typing, with operation `_name` argument;
+ *
+ *    from:
+ *      ```
+ *      import { TYPE_FOR_OPERATION_DATA, TYPE_FOR_OPERATION_VARIABLE } from 'somewhere'
+ *      import { useQuery } from '@apollo/client'
+ *
+ *      const result = useQuery<TYPE_FOR_OPERATION_DATA, TYPE_FOR_OPERATION_VARIABLES>(query, options)
+ *      ```
+ *
+ *    to:
+ *      ```
+ *      import { useQuery } from '~api/client'
+ *
+ *      const result = useQuery('OPERATION_NAME', query, options)
+ *      ```
+ *
+ * 2 - Automatic authorization token based refetch.
+ *    When authenticating (or logging off) most queries should update. We could
+ *    be more intelligent then that, but this is good enough for current stage
+ *    of the application.
+ */
 const useQuery = <
   OperationName extends keyof Queries,
   Types extends Queries[OperationName] = Queries[OperationName]
@@ -32,6 +57,26 @@ const useQuery = <
   return result
 }
 
+/**
+ * This is a enhanced version of Apollo's useMutation with the following purposes:
+ *
+ * 1 - Less verbose typing, with operation `_name` argument;
+ *
+ *    from:
+ *      ```
+ *      import { TYPE_FOR_OPERATION_DATA, TYPE_FOR_OPERATION_VARIABLE } from 'somewhere'
+ *      import { useMutation } from '@apollo/client'
+ *
+ *      const result = useMutation<TYPE_FOR_OPERATION_DATA, TYPE_FOR_OPERATION_VARIABLES>(mutation, options)
+ *      ```
+ *
+ *    to:
+ *      ```
+ *      import { useMutation } from '~api/client'
+ *
+ *      const result = useMutation('OPERATION_NAME', mutation, options)
+ *      ```
+ */
 const useMutation = <
   OperationName extends keyof Mutations,
   Types extends Mutations[OperationName] = Mutations[OperationName]
