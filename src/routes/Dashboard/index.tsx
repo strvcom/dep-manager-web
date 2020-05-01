39ba9cbaaa727ba920b62c9191e43a404b8b4a2f
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2020-05-01 12:02:09 +0200

    refactor: moved dashboard content out of route main file

--- src/routes/Dashboard/index.tsx
+++ src/routes/Dashboard/index.tsx
@@ -1,112 +1,30 @@
-import React, { Suspense, memo, useState } from 'react'
-import { Route, RouteComponentProps, Switch } from 'react-router-dom'
-import ErrorBoundary from 'react-error-boundary'
+import React, { memo, useState } from 'react'
+import { RouteComponentProps } from 'react-router-dom'
+import { GT } from '~api/client'
 
-import * as routes from '../routes'
-import Loading from '~app/components/Loading'
-import NodeLibrariesTable from '~app/components/NodeLibrariesTable'
-import NodeProjectsTable from '~app/components/NodeProjectsTable'
-import ActualityWidget from '~app/components/ActualityWidget'
-import RecentUpdates from '~app/components/RecentUpdates'
-
-import ProjectsOverviewWidget from './ProjectsOverviewWidget'
 import DashboardToolBar from './DashboardToolBar'
-import { TableContainer, StyledDashboard, WidgetContainer } from './styled'
-import { extractLibrariesInfo, filterProjectsBySearch } from './helpers'
+import { DashboardContent } from './DashboardContent'
+import { StyledMain } from './styled'
 
-import { useQuery, GT } from '~api/client'
-import { SemverOutdateStatus as distances } from '~app/@generated/types'
-
-import document from './query.gql'
-
-export type DashboarProps = RouteComponentProps<{
+const Dashboard: React.FC<RouteComponentProps<{
   department: string
   category: string
-}>
-
-const Dashboard = ({ match }: DashboarProps) => {
-  const { department, category } = match.params
+}>> = ({ match }) => {
   const [search, setSearch] = useState('')
-  const cacheKey = department + search
-
-  const { data, loading, error } = useQuery('DASHBOARD_QUERY', document, {
-    variables: { department: department.toUpperCase() as GT.BidaDepartment },
-  })
+  const department = match.params.department.toUpperCase() as GT.BidaDepartment
 
   return (
     <>
       <DashboardToolBar
         department={department}
-        category={category}
+        category={match.params.category}
         search={search}
         setSearch={setSearch}
       />
 
-      <StyledDashboard>
-        {(() => {
-          if (error) throw error
-          if (loading) return <Loading />
-          if (!data) return null
-
-          const { projects, archived } = data
-
-          const { libraries, uniqueLibraries, outdates, recentlyUpdated } = extractLibrariesInfo(
-            projects.edges as any
-          )
-          const { [distances.MAJOR]: major } = outdates
-
-          const renderWidgets = () => (
-            <WidgetContainer>
-              <ProjectsOverviewWidget
-                total={projects.total}
-                archived={archived.total}
-                width="32%"
-              />
-
-              <ActualityWidget
-                title="Libraries Actuality"
-                width="32%"
-                outdated={major.length}
-                total={libraries.length}
-              />
-
-              <RecentUpdates libraries={recentlyUpdated} width="32%" />
-            </WidgetContainer>
-          )
-
-          const renderLibraries = () => {
-            const filtered = uniqueLibraries.filter(({ package: { name } }) =>
-              name.includes(search)
-            )
-            return (
-              <NodeLibrariesTable libraries={filtered} outdates={outdates} cacheKey={cacheKey} />
-            )
-          }
-
-          const renderProjects = () => {
-            const filtered = filterProjectsBySearch(search)(projects.edges as any)
-            return (
-              <NodeProjectsTable department={department} projects={filtered} cacheKey={cacheKey} />
-            )
-          }
-
-          return (
-            <Suspense fallback={<Loading />}>
-              <ErrorBoundary>
-                <TableContainer>
-                  <Route exact path={routes.dashboard} render={renderWidgets} />
-
-                  <Switch>
-                    <Route exact path={routes.libraries} render={renderLibraries} />
-
-                    <Route exact path={routes.projects} render={renderProjects} />
-                  </Switch>
-                </TableContainer>
-              </ErrorBoundary>
-            </Suspense>
-          )
-        })()}
-      </StyledDashboard>
+      <StyledMain>
+        <DashboardContent department={department} search={search} />
+      </StyledMain>
     </>
   )
 }
